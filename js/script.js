3df6018e32b426673ca1238761561416dd62f580
RenatoConfuorto <renato.confuorto@outlook.it>
2022-04-26 18:44:09 +0200

    Esercizio base + bonus 1

--- js/script.js
+++ js/script.js
@@ -6,12 +6,37 @@ const actionBtn = document.getElementById('action-btn');
 //dati inseriti dall'utente da raccogliere
 let difficulty;
 let gridSize;
+let cellsNumber;
+let safeCells;
 
-const activeCells = [];
+let activeCells = [];
 //generare la griglia al click del bottone
 actionBtn.addEventListener('click', startGame);
 
+//generare le bombe
+const bombCount = 16;
+let bombPosition = [];
+let foundBomb = false;
+
+function generateBomb(number, size){
+  while(bombPosition.length < number){
+    const randomNumber = generateRandomInteger(1, size);
+    if(!bombPosition.includes(randomNumber)){
+      bombPosition.push(randomNumber);
+    }
+  }
+}
+
+
+function generateRandomInteger(min, max){
+  return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
 function startGame(){
+  //reimpostare i valori di partenza se si comincia una nuova partita
+  foundBomb = false;
+  activeCells = [];
+  bombPosition = [];
   //raccogliere i dati dell'utente e creare la griglia
   difficulty = difficultyDisplay.value;
   switch(difficulty){
@@ -29,7 +54,13 @@ function startGame(){
   
   }
 
+  cellsNumber = gridSize * gridSize;
+  safeCells = cellsNumber - bombCount;
+
   createGrid();
+  generateBomb(bombCount, cellsNumber);
+  console.log(bombPosition);
+  
 }
 
 
@@ -42,7 +73,6 @@ function createGrid(){
   grid.classList.add('container');
   grid.setAttribute('id', 'grid');
   //creare le celle
-  const cellsNumber = gridSize * gridSize;
   createCells(grid, cellsNumber);
 
   main.append(grid);
@@ -54,22 +84,36 @@ function createCells(element, number){
     cell.classList.add('cell');
     cell.innerHTML = i + 1;
 
-    cell.addEventListener('click', function(){
-      this.classList.add('active');
-      activeCells.push(cell);
-
-      //controllare se sono finite le celle cliccabili
-      checkForWin(activeCells, cellsNumber);
-    });
+    cell.addEventListener('click', clickEvent);
 
     element.append(cell);
   }
 }
 
-function checkForWin(array, number){
 
-  if(array.length === number){
-    alert('Hai Vinto');
+function clickEvent(){
+  const activatedCellNumber = parseInt(this.textContent);
+  //interrompere se è già stata cliccata
+  console.log('cella cliccata');
+  //controllare se cìè la bomba
+  if(bombPosition.includes(activatedCellNumber)){
+    this.classList.add('active', 'bomb');
+    foundBomb = !foundBomb;
+    endGame();
+  }else{
+    this.classList.add('active');
+    activeCells.push(this);
+    endGame();
+  }
+}
+
+
+function endGame(){
+  if(activeCells.length === safeCells){
+    main.innerHTML += `<h2>Hai Vinto!!!</h2>`
   }
 
+  if(foundBomb){
+    main.innerHTML += `<h2>Hai Perso! Hai scoperto ${activeCells.length} celle su ${safeCells}</h2>`
+  }
 }
